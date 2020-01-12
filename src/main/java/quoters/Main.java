4ddc039d5f366fa06ccccb12b50828266f0ebb40
prokopenya <igorprokopenya@gmail.com>
2020-01-12 15:11:16 +0300

    I'll be back

---
+++ src/main/java/quoters/Main.java
@@ -0,0 +1,11 @@
+package quoters;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(TerminateQuoter.class).sayQuote();
+    }
+}
+
---
+++ src/main/java/quoters/TerminateQuoter.java
@@ -0,0 +1,14 @@
+package quoters;
+
+public class TerminateQuoter implements Quoter {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        System.out.println("message = " + message);
+    }
+}
